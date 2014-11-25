7bf4a75a9f6f9422191e48f7a0ca15911ced4a34
Ryan Mahler <ryantmer@gmail.com>
2014-11-25 14:56:41 -0500

    Problem 18 solution

---
+++ euler18.py
@@ -0,0 +1,33 @@
+small = [[3],
+        [7, 4],
+        [2, 4, 6],
+        [8, 5, 9, 3]]
+
+big =  [[75],
+        [95, 64],
+        [17, 47, 82],
+        [18, 35, 87, 10],
+        [20, 4, 82, 47, 65],
+        [19, 1, 23, 75, 3, 34],
+        [88, 2, 77, 73, 7, 63, 67],
+        [99, 65, 4, 28, 6, 16, 70, 92],
+        [41, 41, 26, 56, 83, 40, 80, 70, 33],
+        [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+        [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+        [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+        [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+        [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+        [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]]
+
+
+def euler18(triangle):
+    """Finds the route with the greatest sum down triangle."""
+    for row in range(len(triangle) - 2, -1, -1):  # Starting from the second-last row
+        for col in range(len(triangle[row])):
+            # Replace each entry with itself + max value below it
+            # this bubbles the max path value to the top
+            triangle[row][col] = triangle[row][col] + max(triangle[row+1][col], triangle[row+1][col+1])
+
+    return triangle[0][0]
+
+print euler18(big)
