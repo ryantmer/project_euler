d2b0260c6287e89b88872a6c672f8b7e727f0043
Ryan Mahler <ryantmer@gmail.com>
2015-01-16 12:33:28 -0500

    Added p23 solution, and updated p21 and utlity module

--- euler21.py
+++ euler21.py
@@ -10,12 +10,10 @@ def euler21(n):
     amicable_sum = 0
 
     for num in range(1, n):
-        div_1 = projecteuler.find_divisors(num)
-        div_1.remove(num)  # find_divisors includes the input value, we don't want this
-        div_sum_1 = sum(set(div_1))  # set() will remove duplicates
-        div_2 = projecteuler.find_divisors(div_sum_1)
-        div_2.remove(div_sum_1)
-        div_sum_2 = sum(set(div_2))
+        div_1 = projecteuler.proper_divisors(num)
+        div_sum_1 = sum(div_1)
+        div_2 = projecteuler.proper_divisors(div_sum_1)
+        div_sum_2 = sum(div_2)
         if num == div_sum_2 and num != div_sum_1:
             amicable_sum += num
 
--- projecteuler.py
+++ projecteuler.py
@@ -23,6 +23,16 @@ def is_prime(number):
 
 def find_divisors(number):
     """Returns a list of factors of number."""
-    return reduce(list.__add__, 
-                ([i, number//i] for i in range(1, int(number**0.5) + 1) if number % i == 0))
+    div_pairs = [[i, number//i] for i in range(1, int(number**0.5)+1) if number % i == 0]
+    return reduce(list.__add__, div_pairs)
+
+def proper_divisors(number):
+    """Returns a set of proper divisors of a number (excluding the number itself)."""
+    if number == 1:
+        div = [1]
+        return set(div)
+    div_pairs = [[i, number//i] for i in range(1, int(number**0.5)+1) if number % i == 0]
+    div = reduce(list.__add__, div_pairs)
+    div.remove(number)
+    return set(div)
 ##### Helper functions #####
